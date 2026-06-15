68607852ea09ed1a951a3b5254c2850b8760e05e
footlongdingledon <139654481+footlongdingledon@users.noreply.github.com>
2026-06-15 21:11:24 +0100

    Add files via upload

---
+++ blurrylatro/README.md
@@ -0,0 +1,13 @@
+i had to make this anyway for my own texture pack sooo... i figured i may as well clean it up and post it as a resource
+
+on its own, this does nothing, since it is a texture pack that replaces all the sprites in the game with themselves. it is meant to be edited, to remove a lot of the setup from making a texture pack from scratch
+
+idk if the code is 100% cause im not a lua user but i havent had any issues with it... yet... i used the steamodded example mods as reference
+
+get steamodded here https://github.com/Steamopollys/Steamodded
+
+HOW TO USE:
+- just edit the textures in your image editor of choice, make sure to copy your edits between the 1x and 2x folder, just upscale your 1x stuff by 200% and put it in 2x
+- keep the freakin image dimensions the same as the originals or you might break something!!!
+- if you delete an image, you will need to remove any lines referencing it from the lua file
+- once you want to test it, just install it in the same way as any other mod
---
+++ blurrylatro/main.lua
@@ -0,0 +1,137 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: blurrylatro
+--- MOD_ID: blurrylatro
+--- PREFIX: blurrylatro
+--- MOD_AUTHOR: [footlongdingledong]
+--- MOD_DESCRIPTION: balatro but blurry
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture({
+    key = 'blurry_joker',
+  set = 'Joker',
+    path = 'blurry_jokers.png',
+  loc_txt = {
+      name = 'Blurry Jokers'
+  }
+})
+AltTexture({
+    key = 'blurry_tarots',
+  set = 'Tarot',
+    path = 'blurry_tarots.png',
+  loc_txt = {
+      name = 'Blurry Tarots'
+  }
+})
+AltTexture({
+    key = 'blurry_planet',
+  set = 'Planet',
+    path = 'blurry_tarots.png',
+  loc_txt = {
+      name = 'Blurry Planets'
+  }
+})
+AltTexture({
+    key = 'blurry_spectral',
+  set = 'Spectral',
+    path = 'blurry_tarots.png',
+    soul = 'blurry_decks.png',
+  loc_txt = {
+      name = 'Blurry Spectrals'
+  }
+})
+AltTexture({
+  key = 'blurry_enhance',
+  set = 'Enhanced',
+  path = 'blurry_decks.png',
+  loc_txt = {
+      name = 'Blurry Enhancements'
+  }
+})
+AltTexture({
+  key = 'blurry_seal',
+  set = 'Seal',
+  path = 'blurry_decks.png',
+  loc_txt = {
+      name = 'Blurry Seals'
+  }
+})
+AltTexture({
+  key = 'blurry_deck',
+  set = 'Back',
+  path = 'blurry_decks.png',
+  loc_txt = {
+      name = 'Blurry Decks'
+  }
+})
+AltTexture({
+  key = 'blurry_voucher',
+  set = 'Voucher',
+  path = 'blurry_vouchers.png',
+  loc_txt = {
+      name = 'Blurry Vouchers'
+  }
+})
+AltTexture({
+  key = 'blurry_booster',
+  set = 'Booster',
+  path = 'blurry_boosters.png',
+  loc_txt = {
+      name = 'Blurry Booster Packs'
+  }
+})
+AltTexture({
+  key = 'blurry_tag',
+  set = 'Tag',
+  path = 'blurry_tags.png',
+  loc_txt = {
+      name = 'Blurry Tags'
+  }
+})
+AltTexture({
+  key = 'blurry_blind',
+  set = 'Blind',
+  path = 'blurry_blinds.png',
+  frames = 21,
+  loc_txt = {
+      name = 'Blurry Blinds'
+  }
+})
+AltTexture({
+  key = 'blurry_stake',
+  set = 'Stake',
+  path = 'blurry_stakes.png',
+  loc_txt = {
+      name = 'Blurry Stakes'
+  }
+})
+AltTexture({
+  key = 'blurry_sticker',
+  set = 'Sticker',
+  path = 'blurry_stickers.png',
+  loc_txt = {
+      name = 'Blurry Stickers'
+  }
+})
+TexturePack({
+  key = 'Blurrylatro',
+  textures = {
+      'blurrylatro_blurry_joker',
+      'blurrylatro_blurry_tarots',
+      'blurrylatro_blurry_planet',
+      'blurrylatro_blurry_spectral',
+      'blurrylatro_blurry_enhance',
+      'blurrylatro_blurry_seal',
+      'blurrylatro_blurry_deck',
+      'blurrylatro_blurry_voucher',
+      'blurrylatro_blurry_booster',
+      'blurrylatro_blurry_tag',
+      'blurrylatro_blurry_blind',
+      'blurrylatro_blurry_stake',
+      'blurrylatro_blurry_sticker',
+  },
+  loc_txt = {
+      name = 'Blurrylatro',
+      text = {'i cant see shit'}
+  }
+})
